891c6e3cc9968acd12a17ba7b33e2de21f1a1dde
roman <90575799+roman41k@users.noreply.github.com>
2022-11-13 22:21:12 +0200

    create consoleGroup.js

---
+++ consoleGroup.js
@@ -0,0 +1,8 @@
+console.group('User Details');
+console.log('name: John Doe');
+console.log('job: Software Developer');// Nested Group
+console.group('Address');
+console.log('Street: 123 Townsend Street');
+console.log('City: San Francisco');
+console.log('State: CA');
+console.groupEnd();console.groupEnd();
