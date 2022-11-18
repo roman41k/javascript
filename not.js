0a371736519da29e7a41902af228bba8fa9873aa
roman <90575799+roman41k@users.noreply.github.com>
2022-11-18 16:02:49 +0200

    end of first module konspekt

---
+++ not.js
@@ -0,0 +1,9 @@
+console.log(!true);
+console.log(!false);
+console.log(!3);
+console.log(!"Mango");
+console.log(!0);
+console.log(!"");
+
+const isOnline = true;
+const isNotOnline = !isOnline;
---
+++ or.js
@@ -0,0 +1,17 @@
+//const age = 5;
+//console.log(age < 10 || age > 30);
+
+//const age = 40;
+//console.log(age < 10 || age > 30);
+
+const age = 20;
+console.log(age < 10 || age > 30);
+
+console.log(true || false);
+console.log(false || true);
+console.log(true || true);
+
+console.log(3 || false);
+console.log(false || 3);
+console.log(3 || true);
+console.log(true || 3);
