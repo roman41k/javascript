9886e3d9cf08fcbc8eb47176eff804207d9de7e5
roman <90575799+roman41k@users.noreply.github.com>
2022-11-18 15:06:26 +0200

    javascript_learning

---
+++ and2.js
@@ -0,0 +1,8 @@
+console.log(1 && 5);
+console.log(5 && 1);
+console.log(0 && 2);
+console.log(2 && 0);
+console.log("" && "Mango");
+console.log("Mango" && "");
+console.log("Mango" && "Poly");
+console.log("Poly" && "Mango");
---
+++ and_false.js
@@ -0,0 +1,3 @@
+const age = 50;
+console.log(age > 10 && age < 30);
+console.log(age > 80 && age < 120);
