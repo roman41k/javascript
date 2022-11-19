75b0fb3fea13ec18ed3c247f644df918ce608316
roman <90575799+roman41k@users.noreply.github.com>
2022-11-19 09:05:14 +0200

    create if.js

---
+++ if.js
@@ -0,0 +1,17 @@
+/*let cost = 0;
+const subscription = "pro";
+
+if (subscription === "pro") {
+   cost = 100;
+}
+
+console.log(cost); */	
+
+cost = 0;
+const subscription = "free";
+
+if (subscription === "pro") {
+   cost = 100;
+}
+
+console.log(cost);
