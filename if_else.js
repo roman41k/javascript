4c9c150b96a7ee8dcbc41fca42fe9ac911f08e9c
roman <90575799+roman41k@users.noreply.github.com>
2022-11-19 17:58:44 +0200

    create if_else.js

---
+++ if_else.js
@@ -0,0 +1,21 @@
+/*let cost;
+const subscription = "free";
+
+if (subscription === "pro") {
+   cost = 100;
+} else {
+   cost = 0;
+}
+
+console.log(cost); */
+
+let cost;
+const subscription = "pro";
+
+if (subscription === "pro") {
+   cost = 100;
+} else {
+   cost = 0;
+}
+
+console.log(cost);
