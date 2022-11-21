e82f259598e95e4600b0dcd449068a6ddd35dd5c
roman <90575799+roman41k@users.noreply.github.com>
2022-11-21 07:58:11 +0200

    lesson20.11.2022

--- else_if.js
+++ else_if.js
@@ -1,7 +1,14 @@
 let cost;
-const subscription = "preminum";
+const subscription = "premium";
 
 if (subscription === "free") {
    cost = 0;
-} else if (subscribtion === "pro") {
+} else if (subscription === "pro") {
    cost = 100;
+} else if (subscription === "premium") {
+   cost = 500;
+} else {
+   console.log("Invalid subscription type");
+}
+
+console.log(cost);
---
+++ ternary_operator.js
@@ -0,0 +1,33 @@
+/*let type;
+const age = 20;
+
+if (age >= 18) {
+   type = "adult";
+   
+} else {
+   type = "child";
+}
+
+console.log(type);
+
+const age = 20;
+const type = age >= 18 ? "adult" : "child";
+console.log(type); 
+
+const num1 = 5;
+const num2 = 10;
+let biggerNumber;
+
+if (num1 > num2) {
+   biggerName = num1;
+} else {
+   biggerName = num2;
+}
+
+console.log(biggerName); */
+
+const num1 = 5;
+const num2 = 10;
+const biggerNumber = num1 > num2 ? num1 : num2;
+
+console.log(biggerNumber);
