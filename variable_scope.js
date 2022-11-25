5625bb302bfa754fb9a07b1e62ffa012b96e5721
roman <90575799+roman41k@users.noreply.github.com>
2022-11-25 17:18:58 +0200

    lesson 25.11.2022

--- variable_scope.js
+++ variable_scope.js
@@ -4,11 +4,50 @@ if (true) {
    console.log("Block scope: ", value);
 }
 
-console.log("Global scope: ", value);*/
+console.log("Global scope: ", value);
 
 if (true) {
    const value = 5;
    console.log("Block scope: ", value);
 }
 
-console.log("Global scope: ", value);
+console.log("Global scope: ", value);*/
+
+const global = "global";
+
+if (true) {
+   const blockA = "block A";
+   
+   console.log(global);
+   console.log(blockA);
+   
+   /*console.log(blockB);
+   console.log(blockC);*/
+   
+   if (true) {
+      const blockB = "block B";
+      
+      console.log(global);
+      console.log(blockA);
+      console.log(blockB);
+      
+      /*console.log(blocC);*/
+   }
+}
+
+if (true) {
+   const blockC = "blockC";
+   
+   console.log(global);
+   console.log(blockC);
+   
+   //console.log(blockA);
+   //console.log(blockB);
+}
+
+console.log(global);
+
+/*console.log(blockA);
+console.log(blockB);
+console.log(blockC);*/
+
