c84974fdde5cef175d14ad440b7e75a2e17a69a4
roman <90575799+roman41k@users.noreply.github.com>
2022-11-27 22:20:04 +0200

    lesson 27.11.2022

---
+++ while.js
@@ -0,0 +1,14 @@
+/*let counter =0;
+
+while (counter < 25) {
+   console.log("counter: ", counter);
+   counter += 1;
+}*/
+
+let clientCounter = 18;
+const maxClients = 25;
+
+while (clientCounter < maxClients) {
+   console.log(clientCounter);
+   clientCounter += 1;
+}
