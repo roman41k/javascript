01033c69aff3d21a6dc80e65717b15f99e310f1a
roman <90575799+roman41k@users.noreply.github.com>
2022-12-09 13:25:29 +0200

    lesson 09.12.2022

---
+++ do_while.js
@@ -0,0 +1,15 @@
+/*let password = "";
+
+do {
+   password = prompt("Enter a password longer than 4 characters", "");
+}  while (password.length < 5);
+
+console.log("Password entered: ", password);*/
+
+let password = "";
+
+do {
+  password = prompt("Введіть пароль довший 4-х символів", "");
+} while (password.length < 5);
+
+console.log("Ввели пароль: ", password);
---
+++ for.js
@@ -0,0 +1,17 @@
+/*for (let i = 0; i <= 20; i +=5) {
+   console.log(i);
+}
+
+const target = 3;
+let sum = 0;
+
+for (let i = 0; i <= target; i += 1) {
+   sum += i;
+}
+
+console.log(sum);*/
+
+const max = 10;
+for (let i = 0; i < max; i += 1) {
+   console.log(`${max} % ${i} = `, max % i);
+}
